f7d87dbb2e2bf0f44128c8706528c44adeab520b
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2023-01-16 01:28:54 +0530

    [Shubham]Added. Player is at Starting Position Zero

---
+++ Snake_Ladder/snake_ladder.py
@@ -0,0 +1,14 @@
+# UC1- Single Player at Starting Position Zero
+
+def snake_ladder():
+    """
+        This function checks for the starting Position of Single player
+    :return: None
+    """
+    player_position = 0
+    print("Lets Start the Game")
+    print("Player is at Start Position ", player_position)
+
+
+if __name__ == '__main__':
+    snake_ladder()
