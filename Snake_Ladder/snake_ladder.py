55b1afd40d81ded7ff5a434b5a6337c323209e62
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2023-01-16 13:32:58 +0530

    [Shubham]Added. Die_roll till Winning Position is reached

--- Snake_Ladder/snake_ladder.py
+++ Snake_Ladder/snake_ladder.py
@@ -3,31 +3,31 @@ import random
 
 def snake_ladder():
     """
-        This function checks the options for snake,ladder or no play and updates the player
-        position accordingly
+        This function Checks for the player position to reach winning position 100
+        If reached then will print that player won the game
     :return: None
     """
-    player_position = 0
-    print("Lets Start the Game")
-    print("Player is at Start Position ", player_position)
-    print("Lets Roll the Dice")
-    number_on_dice = roll_dice()
-    print("The Number on Dice is:", number_on_dice)
-    player_options = random.randint(0, 2)
     # no_play = 0
     # ladder = 1
     # snake = 2
-    match player_options:
-        case 0:
-            player_position = 0
-        case 1:
-            player_position += number_on_dice
-        case 2:
-            player_position -= number_on_dice
-            if player_position < 0:
-                player_position = 0
+    player_position = 0
+    winning_position = 100
+    print("Lets Start the Game")
+    while player_position < winning_position:
+        number_on_dice = roll_dice()
+        player_options = random.randint(0, 2)
+        match player_options:
+            case 0:
+                player_position += 0
+            case 1:
+                player_position += number_on_dice
+            case 2:
+                player_position -= number_on_dice
+                if player_position < 0:
+                    player_position = 0
+        print("Player Position is: ", player_position)
 
-    print("Player Position is:", player_position)
+    print("Congratulations You Won!")
 
 
 def roll_dice():
